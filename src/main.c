18d3225190d86ba66ba1ef39f19195a8c000e875
developer4 <support@d-logic.rs>
2016-07-26 10:59:13 +0200

    Cosmetic changes in main.c

--- src/main.c
+++ src/main.c
@@ -33,38 +33,39 @@ int main(int argc, char**argv)
 
 	baud_rate = get_linux_baudrate(DEFAULT_BAUD_RATE);
 
-	while ((c = getopt(argc, argv, "fvs:c:")) != -1)
-	switch (c) {
-	case 'f':
-		fprintf(stderr, "softwedge not daemonizing...\n");
-		dontDaemon = 1;
-		break;
-	case 'v':
-		fprintf(stderr, "Application version - %s: linux-uart-keyboard-emualation for X11.\n"
-				"uFR device must be connected using FTDI usb to serial driver (add user to dialout group).\n"
-				"uFR device must be in asynchronous UID mode.\n", APP_VERSION);
-		fprintf(stderr, "(Exiting...)\n");
-		exit(0);
-	case 's':
-		baud_rate = get_linux_baudrate(atoi(optarg));
-		if (!baud_rate) {
-			fprintf (stderr, "Using default baud rate (%d bps).\n", DEFAULT_BAUD_RATE);
-			baud_rate = get_linux_baudrate(DEFAULT_BAUD_RATE);
+	while ((c = getopt(argc, argv, "fvs:c:")) != -1) {
+		switch (c) {
+			case 'f':
+				fprintf(stderr, "softwedge not daemonizing...\n");
+				dontDaemon = 1;
+				break;
+			case 'v':
+				fprintf(stderr, "Application version - %s: linux-uart-keyboard-emualation for X11.\n"
+						"uFR device must be connected using FTDI usb to serial driver (add user to dialout group).\n"
+						"uFR device must be in asynchronous UID mode.\n", APP_VERSION);
+				fprintf(stderr, "(Exiting...)\n");
+				exit(0);
+			case 's':
+				baud_rate = get_linux_baudrate(atoi(optarg));
+				if (!baud_rate) {
+					fprintf (stderr, "Using default baud rate (%d bps).\n", DEFAULT_BAUD_RATE);
+					baud_rate = get_linux_baudrate(DEFAULT_BAUD_RATE);
+				}
+				break;
+			case 'c':
+				sport = optarg;
+				break;
+			case '?':
+				if (optopt == 'c' || optopt == 's')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				return 1;
+			default:
+				abort ();
 		}
-		break;
-	case 'c':
-		sport = optarg;
-		break;
-	case '?':
-		if (optopt == 'c' || optopt == 's')
-			fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-		else if (isprint (optopt))
-			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-		else
-			fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-		return 1;
-	default:
-		abort ();
 	}
 
 	if (sport == NULL)
